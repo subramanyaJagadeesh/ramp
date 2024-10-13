f402e80981aba396c9da5201b37237b1a2cb2686
subramanyajagadeesh <subramanya.grj@gmail.com>
2024-10-12 23:01:02 -0700

    Fixed bug 4

--- src/hooks/usePaginatedTransactions.ts
+++ src/hooks/usePaginatedTransactions.ts
@@ -22,7 +22,7 @@ export function usePaginatedTransactions(): PaginatedTransactionsResult {
         return response
       }
 
-      return { data: response.data, nextPage: response.nextPage }
+      return { data: previousResponse.data.concat(response.data), nextPage: response.nextPage }
     })
   }, [fetchWithCache, paginatedTransactions])
 
